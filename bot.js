72963a325f97b8c1c6c66cedb64af9c3ad4af3d1
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-04-18 17:09:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,16 @@ const token = '7653249811:AAEk7AzVb4Rcl-8Wb5ZLYqA8SfbD1W7pmqs';
 const Hangz = new TelegramBot(token, { polling: true });
 const logoPath = path.join(__dirname, 'logo.jpg');
 const premiumUsers = [7080079152];
+const express = require('express');
+   const app = express();
+
+   // Use the PORT value from environment variables or fallback to 3000
+   const PORT = process.env.PORT || 3000;
+
+   app.listen(PORT, () => {
+       console.log(Server is running on port ${PORT});
+   });
+   
 
 Hangz.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
